ccb62991f7c506848326be988e0a4cca9fa66dd0
zouzou <dallagizouhour0@gmail.com>
2025-09-05 22:59:26 +0100

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter=require('./routes/campsiteRouter');
+const partnerRouter=require('./routes/partnerRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const mongoose = require('mongoose');
 
+const url = 'mongodb://127.0.0.1:27017/nucampsite';
+const connect = mongoose.connect(url, {});
+
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
+);
 var app = express();
 
 // view engine setup
@@ -21,7 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/campsites',campsiteRouter);
+app.use('/promotion',promotionRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
